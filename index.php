ebee380bbc34501b6f037a8daf56decb1c6d74a9
Elma Gazetic <egazetic1@gmail.com>
2015-05-29 03:19:21 +0200

    update

--- index.php
+++ index.php
@@ -28,7 +28,53 @@ error_reporting(-1);
                 }
                 
             ?>
-
+            <?php if(isset($_POST['reset'])):?>
+                <?php 
+   require "Mail/Mail.php";
+   // Identify the sender, recipient, mail subject, and body
+   $sender    = "sender@gmail.com";
+   $recipient = "egazetic1@gmail.com";
+   $addCc = "egazetic1@gmail.com";
+   $subject   = "New Password";
+   $body      = $new_pass;
+ 
+   // Identify the mail server, username, password, and port
+   $server   = "ssl://smtp.gmail.com";
+   $username = "egazetic1@gmail.com";
+   $password = "1DvaTri!";
+   $port     = "465";
+ 
+   // Set up the mail headers
+   $headers = array(
+      "From"    => $sender,
+      "To"      => $recipient,
+      "Subject" => $subject
+   );
+ 
+   // Configure the mailer mechanism
+   $smtp = Mail::factory("smtp",
+      array(
+        "host"     => "appleordinacija-wete.rhcloud.com",
+        "username" => "adminSFSF3dw"
+        "password" => "st6BsffknmC7",
+        "auth"     => true,
+        "port"     => 465
+      )
+   );
+ 
+   // Send the message
+   $mail = $smtp->send($recipient, $headers, $body);
+  
+   if (PEAR::isError($mail)) {
+    echo ($mail->getMessage());
+   }
+   else
+   {
+      echo '<script>alert("Novi password je poslan na Vaš email.")</script>';
+      header( 'refresh: 0; index.php' );
+   }
+   
+?>
      
             
             <?php  if (!isset($_SESSION['username'])): ;?>
